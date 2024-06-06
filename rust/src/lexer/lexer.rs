9c722dafe8f6e7f574bcc52d6c526812a920bfa7
sonhmai <>
2024-06-06 12:52:03 +0700

    rust: add a failed lexer test

--- rust/src/lexer/lexer.rs
+++ rust/src/lexer/lexer.rs
@@ -17,3 +17,31 @@ impl Lexer {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::lexer::Lexer;
+    use crate::token::Token;
+
+    fn assert_correct_tokens(code: &str, expected_tokens: Vec<Token>) {
+        let mut lexer = Lexer::new(code);
+        let mut token;
+        for expected in expected_tokens {
+            token = lexer.next_token();
+            assert_eq!(token, expected);
+        }
+    }
+
+    #[test]
+    fn test_lexer_should_parse_correct_tokens() {
+        assert_correct_tokens(
+            "let s=5;",
+            vec![
+                Token::Keyword("let".to_string()),
+                Token::Identifier("s".to_string()),
+                Token::EqualSign,
+                Token::Int(5),
+                Token::EOF,
+            ]
+        );
+    }
+}
