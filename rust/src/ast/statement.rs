247dc13b9eae51f6c269a7dbbe1ccf2240a0465a
sonhmai <14060682+sonhmai@users.noreply.github.com>
2024-06-08 10:51:00 +0700

    impl eval let stmt and hit compiler error with string

--- rust/src/ast/statement.rs
+++ rust/src/ast/statement.rs
@@ -2,15 +2,15 @@ use crate::ast::{Expression, Identifier};
 
 #[derive(Debug)]
 pub struct LetStatement {
-    pub name: Identifier,
-    pub value: Expression,
+    pub identifier: Identifier,
+    pub expression: Expression,
 }
 
 impl LetStatement {
     pub fn new(identifier: Identifier, expression: Expression) -> Self {
         LetStatement {
-            name: identifier,
-            value: expression,
+            identifier,
+            expression,
         }
     }
 }
